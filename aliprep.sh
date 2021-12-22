d76d5b9140db5338ffa5786f9903085b392a7c73
Yongkang He <he@yongkang.cloud>
2021-12-22 11:33:52 +0800

    Create aliprep.sh

---
+++ aliprep.sh
@@ -0,0 +1,20 @@
+echo "Install ossutil64"
+wget http://gosspublic.alicdn.com/ossutil/1.7.7/ossutil64                           
+chmod 755 ossutil64
+
+echo "export PATH=$PATH:~/ack-k10" >> ~/.bashrc
+echo "alias k=kubectl" >> ~/.bashrc
+. ~/.bashrc
+
+clear
+
+echo -n "Enter your Alicloud Access Key ID and press [ENTER]: "
+read AccessKeyId
+echo "" | awk '{print $1}'
+echo $AccessKeyId > aliaccess
+echo -n "Enter your Alicloud Access Key Secret and press [ENTER]: "
+read AccessKeySecret
+echo $AccessKeySecret >> aliaccess
+echo "" | awk '{print $1}'
+echo "You are ready to deploy now!"
+echo "" | awk '{print $1}'
