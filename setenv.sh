b04c7dea3784e2b5769fb7b1826f671b37e63092
Yongkang He <he@yongkang.cloud>
2021-12-22 11:34:23 +0800

    Create setenv.sh

---
+++ setenv.sh
@@ -0,0 +1,10 @@
+echo '-------Set the environment variables'
+export MY_CLUSTER=ack4yong1
+export MY_REGION=ap-southeast-1         #Customize your favorite region
+export MY_ZONE=ap-southeast-1a          #Customize your favorite zone
+export MY_VPC_CIDR=172.30.0.0/16        #Customize your favorite VPC CIDR
+export MY_VSWITCH_CIDR=172.30.0.0/24    #Customize your favorite VSWITCH CIDR
+export MY_KEYPAIR=key4yong1             #Customize your favorite Keypair name
+export MY_INSTANCE_TYPE=ecs.g7.xlarge   #Customize your instance type
+export MY_BUCKET=k10bucket4yong1-sg     #Customize your favorite bucket
+export MY_OBJECT_STORAGE_PROFILE=myoss1 #Customize your favorite profile name
