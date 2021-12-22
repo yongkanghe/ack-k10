badfe980a98c1f4cc8235530f1c497ddd038aa85
Yongkang He <he@yongkang.cloud>
2021-12-22 14:06:39 +0800

    Update k10-deploy.sh

--- k10-deploy.sh
+++ k10-deploy.sh
@@ -4,7 +4,7 @@ starttime=$(date +%s)
 
 echo $MY_BUCKET > ack_bucketname
 
-export KUBECONFIG=./ack_kubeconfig
+export KUBECONFIG=~/ack-k10/ack_kubeconfig
 
 echo '-------Install K10'
 kubectl create ns kasten-io
