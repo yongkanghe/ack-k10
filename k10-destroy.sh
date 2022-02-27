8d7a685940bed80296e0c9c38d53479ff1f7298e
Yongkang He <he@yongkang.cloud>
2022-02-27 13:44:18 +0800

    Update k10-destroy.sh

--- k10-destroy.sh
+++ k10-destroy.sh
@@ -3,7 +3,7 @@ starttime=$(date +%s)
 export KUBECONFIG=~/ack-k10/ack_kubeconfig
 
 echo '-------Removing the restorepointcontent of Postgresql'
-kubectl delete restorepointcontent -l k10.kasten.io/appNamespace=yong-postgresql 
+kubectl get restorepointcontent -l k10.kasten.io/appNamespace=yong-postgresql | grep -v NAME | xargs kubectl delete restorepointcontent
 sleep 60
 
 echo '-------Removing Kasten K10 and Postgresql'
