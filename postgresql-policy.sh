19f194795812243e43aa5c1774abd9173c9ff678
Yongkang He <he@yongkang.cloud>
2022-02-27 16:44:09 +0800

    Update postgresql-policy.sh

--- postgresql-policy.sh
+++ postgresql-policy.sh
@@ -42,18 +42,18 @@ spec:
           - yong-postgresql
 EOF
 
-sleep 8
+# sleep 8
 
-echo '-------Kickoff the on-demand backup job'
-cat <<EOF | kubectl create -f -
-apiVersion: actions.kio.kasten.io/v1alpha1
-kind: RunAction
-metadata:
-  generateName: run-backup-
-spec:
-  subject:
-    kind: Policy
-    name: yong-postgresql-backup
-    namespace: kasten-io
-EOF
+# echo '-------Kickoff the on-demand backup job'
+# cat <<EOF | kubectl create -f -
+# apiVersion: actions.kio.kasten.io/v1alpha1
+# kind: RunAction
+# metadata:
+#   generateName: run-backup-
+# spec:
+#   subject:
+#     kind: Policy
+#     name: yong-postgresql-backup
+#     namespace: kasten-io
+# EOF
 
