a5a6e9c3ec9acc6beed7617cd45e170f914ba8d6
Yongkang He <he@yongkang.cloud>
2022-02-27 15:48:41 +0800

    Create redis-deploy.sh

---
+++ redis-deploy.sh
@@ -0,0 +1,4 @@
+echo '------Deploy Redis'
+kubectl create ns yong-redis
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm install redis bitnami/redis -n yong-redis --set master.persistence.size=20Gi
